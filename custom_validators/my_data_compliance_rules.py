949a9f8d8b774ea480045c67983273c239978706
Adam Byczkowski <38091261+qduk@users.noreply.github.com>
2026-05-22 14:53:19 -0500

    Added more rules

--- custom_validators/my_data_compliance_rules.py
+++ custom_validators/my_data_compliance_rules.py
@@ -3,6 +3,7 @@ from nautobot_data_validation_engine.custom_validators import (
     ComplianceError,
     CustomValidatorIterator
 )
+import re
 
 
 class VLANMinMaxComplianceRule(DataComplianceRule):
@@ -25,4 +26,40 @@ class VLANMinMaxComplianceRule(DataComplianceRule):
                 }
             )
         
-custom_validators = [VLANMinMaxComplianceRule]
+class DeviceDataComplianceRules(DataComplianceRule):
+    model = "dcim.device"
+    enforce = False
+
+    # Checks if a device name contains any special characters other than a dash (-), underscore (_), or period (.) using regex
+    def audit_device_name_chars(self):
+        if not re.match("^[a-zA-Z0-9\-_.]+$", self.context["object"].name):
+            raise ComplianceError({"name": "Device name contains unallowed special characters."})
+
+    def audit(self):
+        messages = {}
+        for fn in [self.audit_device_name_chars]:
+            try:
+                fn()
+            except ComplianceError as ex:
+                messages.update(ex.message_dict)
+        if messages:
+            raise ComplianceError(messages)
+
+class RackDeviceComplianceRules(DataComplianceRule):
+    model = "dcim.device"
+    enforce = False
+
+    # Checks if a device is not assigned to a rack
+    def audit_device_rack(self):
+        if not self.context["object"].rack:
+            raise ComplianceError({"rack": "Device should be assigned to a rack."})
+
+    def audit(self):
+        messages = {}
+        for fn in [self.audit_device_rack]:
+            try:
+                fn()
+            except ComplianceError as ex:
+                messages.update(ex.message_dict)
+        if messages:
+            raise ComplianceError(messages)
